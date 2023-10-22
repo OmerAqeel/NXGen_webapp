8c1cb42a107dd549e798dd8b948d745ef5e8a49e
Omer Aqeel <macbookpro@Omer-Aqeel.local>
2023-10-22 17:45:23 +0100

    few changes made

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import OurProjects from './Sections/OurProjects';
 import Cover from './Sections/Cover';
-import OurVision from './Sections/OurVision';
 import OurTeam from './Sections/OurTeam';
 import Contact from './Sections/Contact';
 
@@ -10,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <Cover />
-      <OurVision />
       <OurProjects />
       <OurTeam />
       <Contact />
