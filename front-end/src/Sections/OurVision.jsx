cdea101669a1df95b1c4a838be3a125ef56866ba
Omer Aqeel <macbookpro@Omer-Aqeel.local>
2023-10-22 17:50:44 +0100

    few more adjustments made

--- front-end/src/Sections/OurVision.jsx
+++ front-end/src/Sections/OurVision.jsx
@@ -6,7 +6,7 @@ function OurVision() {
         return (
             <div className="ourVisionContainer">
                 <h1 className='visionTitle'>Our Vision</h1>
-
+                
             </div>
         )
     }
